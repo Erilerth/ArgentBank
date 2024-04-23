6fd4152dfdffbb31118f544f00aed51149ccf831
Tony THERLIER <146343369+Erilerth@users.noreply.github.com>
2024-04-23 17:13:47 +0200

    feat: add routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import './utils/styles/_reset.scss';
+import './utils/styles/style.scss';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import LandingPage from './pages/LandingPage/LandingPage';
+import LoginPage from './pages/LoginPage/LoginPage';
+
 function App() {
-  return <></>;
+  return (
+    <Router>
+      <Routes>
+        <Route exact path='/' element={<LandingPage />} />
+        <Route path='/login' element={<LoginPage />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
