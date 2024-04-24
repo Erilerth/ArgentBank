dd00a1ae5c7e00dedd169d5aaa4f18d459792e18
Tony THERLIER <146343369+Erilerth@users.noreply.github.com>
2024-04-24 17:19:35 +0200

    feat: add FeatureItem component and refactor LandingPage

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,7 +1,6 @@
 import './_landingPage.scss';
-import IconChat from '../../assets/images/landingPage/icon-chat.png';
-import IconMoney from '../../assets/images/landingPage/icon-money.png';
-import IconSecurity from '../../assets/images/landingPage/icon-security.png';
+import FeatureArray from '../../data/featuresArray.json';
+import FeatureItem from '../../components/FeatureItem/FeatureItem';
 
 export default function LandingPage() {
   return (
@@ -17,35 +16,15 @@ export default function LandingPage() {
       </div>
       <section className='features'>
         <h2 className='sr-only'>Features</h2>
-        <div className='feature-item'>
-          <img src={IconChat} alt='Chat Icon' className='feature-icon' />
-          <h3 className='feature-item-title'>You are our #1 priority</h3>
-          <p>
-            Need to talk to a representative? You can get in touch through our
-            24/7 chat or through a phone call in less than 5 minutes.
-          </p>
-        </div>
-        <div className='feature-item'>
-          <img src={IconMoney} alt='Money Icon' className='feature-icon' />
-          <h3 className='feature-item-title'>
-            More savings means higher rates
-          </h3>
-          <p>
-            The more you save with us, the higher your interest rate will be!
-          </p>
-        </div>
-        <div className='feature-item'>
-          <img
-            src={IconSecurity}
-            alt='Security Icon'
-            className='feature-icon'
+        {FeatureArray.map((evt, id) => (
+          <FeatureItem
+            key={id}
+            src={evt.src}
+            alt={evt.alt}
+            title={evt.Title}
+            text={evt.Text}
           />
-          <h3 className='feature-item-title'>Security you can trust</h3>
-          <p>
-            We use top of the line encryption to make sure your data and money
-            is always safe.
-          </p>
-        </div>
+        ))}
       </section>
     </main>
   );
