ff83e120ff7c8049f69cde205a9a4e30a213d6f4
Tony THERLIER <146343369+Erilerth@users.noreply.github.com>
2024-04-24 17:36:30 +0200

    feat: add non-functional LoginPage and ProfilePage + routing for Profile

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import LandingPage from './pages/LandingPage/LandingPage';
 import LoginPage from './pages/LoginPage/LoginPage';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
+import Profile from './pages/Profile/Profile';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Routes>
         <Route exact path='/' element={<LandingPage />} />
         <Route path='/login' element={<LoginPage />} />
+        <Route path='/profile' element={<Profile />} />
       </Routes>
       <Footer />
     </Router>
