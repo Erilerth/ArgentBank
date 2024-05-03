c2cfa305607288376c2837eed099e38a07e35ea9
Tony THERLIER <146343369+Erilerth@users.noreply.github.com>
2024-05-03 03:54:03 +0200

    feat : Add API slice for authentication and authorization

---
+++ src/app/api/apiSlice.js
@@ -0,0 +1,36 @@
+import { CreateApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { setCredentials, logOut } from '../../features/auth/authSlice';
+
+const baseQuery = fetchBaseQuery({
+  baseUrl: 'http://locahost:3001',
+  credentials: 'include',
+  prepareHeaders: (headers, { getState }) => {
+    const token = getState().auth.token;
+    if (token) {
+      headers.set('authorization', `Bearer ${token}`);
+    }
+    return headers;
+  },
+});
+
+const baseQueryWithReauth = async (args, api, extraOptions) => {
+  let result = await baseQuery(args, api, extraOptions);
+
+  if (result?.error?.originalStatus === 403) {
+    console.log('sending refresh token');
+    //send the refresh token to get new access token
+    const refreshResult = await baseQuery('/refresh', api, extraOptions);
+    console.log(refreshResult);
+    if (refreshResult?.data) {
+      const user = api.getState().auth.user;
+      //store the new token
+      api.dispatch(setCredentials({ ...refreshResult.data, user }));
+      //retry the original query with new access toekn
+      result = await baseQuery(args, api, extraOptions);
+    } else {
+      api.dispatch(logOut());
+    }
+  }
+
+  return result;
+};
