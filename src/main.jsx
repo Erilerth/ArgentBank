db7d280d1f54a0da8ef71581684e072918035f20
Tony THERLIER <146343369+Erilerth@users.noreply.github.com>
2024-05-04 17:33:38 +0200

    feat : Add store configuration for Redux and API slice

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
+import { store } from './app/store.js';
 import { Provider } from 'react-redux';
-import { configureStore } from '@reduxjs/toolkit';
-import rootReducer from './reducers';
-
-//REDUX
-const store = configureStore({
-  reducer: rootReducer,
-  devTools: true,
-});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
