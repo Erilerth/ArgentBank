5b41aa71478b33e761d421d7ebe42f0ab6715d18
Tony THERLIER <146343369+Erilerth@users.noreply.github.com>
2024-05-08 17:56:11 +0200

    refactor: update Header component to display user's first name when signed in

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,14 @@
 import './_header.scss';
 import Logo from '../../assets/images/header/argentBankLogo.webp';
+import { useSelector } from 'react-redux';
+import {
+  selectCurrentFirstName,
+  selectCurrentToken,
+} from '../../features/auth/authSlice';
 
 export default function Header() {
+  const token = useSelector(selectCurrentToken);
+  const firstName = useSelector(selectCurrentFirstName);
   return (
     <header>
       <nav className='main-nav'>
@@ -14,15 +21,17 @@ export default function Header() {
           <h1 className='sr-only'>Argent Bank</h1>
         </a>
         <div>
-          <a className='main-nav-item' href='/login'>
+          <a className='main-nav-item' href={token ? '/profile' : '/login'}>
             <i className='fa fa-user-circle'></i>
-            Sign In
+            {token ? firstName : 'Sign In'}
           </a>
 
-          {/* <a className='main-nav-item' href='./index.html'>
-            <i className='fa fa-sign-out'></i>
-            Sign Out
-          </a> */}
+          {token && (
+            <a className='main-nav-item' href='./index.html'>
+              <i className='fa fa-sign-out'></i>
+              Sign Out
+            </a>
+          )}
         </div>
       </nav>
     </header>
